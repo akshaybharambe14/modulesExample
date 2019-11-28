0ad64c7d0d43e45a90eee8945bda9451b66c8d19
Akshay Bharambe <akshaybharambe14@gmail.com>
2019-11-28 23:29:26 +0530

    Update: Shift to new major version completely.
    This shifts application to new major version of direct dependency and removes previous version.

--- hello.go
+++ hello.go
@@ -1,14 +1,13 @@
 package hello
 
 import (
-	"rsc.io/quote"
-	quoteV3 "rsc.io/quote/v3"
+	"rsc.io/quote/v3"
 )
 
 func Hello() string {
-	return quote.Hello()
+	return quote.HelloV3()
 }
 
 func Proverb() string {
-	return quoteV3.Concurrency()
+	return quote.Concurrency()
 }
